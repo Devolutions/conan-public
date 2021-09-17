f0773346d787448735830f3436a17edcb53fa0c7
Marc-André Moreau <mamoreau@devolutions.net>
2021-09-17 11:54:55 -0400

    fix fix

--- merge.ps1
+++ merge.ps1
@@ -3,7 +3,7 @@
 $CacheNames = Get-ChildItem -Directory . -Name
 
 if (Test-Path Env:CONAN_USER_HOME) {
-    $ConanDataPath = Join-Path $Env:CONAN_USER_HOME "data"
+    $ConanDataPath = Join-Path $Env:CONAN_USER_HOME ".conan/data"
 } else {
     $ConanDataPath = ".conan/data"
 }
@@ -19,3 +19,7 @@ foreach ($CacheName in $CacheNames) {
         Copy-Item -Path "./$CacheName/$PackageName/*" -Destination "./$ConanDataPath/$PackageName" -Recurse -Force
     }
 }
+
+# upload all packages in the local cache:
+# $PackageRefs = $(conan search *devolutions/stable --raw).Split([Environment]::NewLine)
+# $PackageRefs | ForEach-Object { conan upload $_ --all -r devolutions-public }
