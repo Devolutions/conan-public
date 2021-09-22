d31a98f42c787891ea04a37a1dd8e918b3b8b8fb
Marc-André Moreau <marcandre.moreau@gmail.com>
2021-09-22 12:19:40 -0400

    improve conan cache export + publish.ps1 script (#11)
    * try using second job to merge conan caches
    
    * try trimming conan cache
    
    * try merging all conan caches
    
    * fix fix
    
    * verbose stuff
    
    * try fixing conan user home
    
    * fix fix
    
    * attempt metadata merge
    
    * fix fix fix
    
    * add local package creation
    
    * disable short path usage
    
    * update
    
    * Use GitHub workspace
    
    * Use tmp
    
    * Move-Item Force
    
    * Don't move Conan cache
    
    * Fix
    
    * Fix fix
    
    * Strip components
    
    * fix merge
    
    * fix tarballs
    
    * cleaner merge
    
    * include entire conan cache
    
    * use publish script
    
    * update publish script
    
    * fix conan timestamps
    
    * recurse conanmanifest.txt
    
    * remove publish step
    
    Co-authored-by: Marc-André Moreau <mamoreau@devolutions.net>

---
+++ publish.ps1
@@ -0,0 +1,69 @@
+#!/usr/bin/pwsh
+
+#$Env:CONAN_REMOTE_NAME="devolutions-public"
+#$Env:CONAN_REMOTE_URL="https://conan.cloudsmith.io/devolutions/conan-public"
+
+function Invoke-TlkPublish {
+	param(
+		[string] $ConanRemoteName,
+		[string] $ConanRemoteUrl,
+        [string] $ConanUsername,
+        [string] $ConanPassword,
+        [switch] $MultiCache
+	)
+
+    if (-Not [string]::IsNullOrEmpty($ConanRemoteName)) {
+        $Env:CONAN_REMOTE_NAME = $ConanRemoteName
+    }
+
+    if (-Not [string]::IsNullOrEmpty($ConanRemoteUrl)) {
+        $Env:CONAN_REMOTE_URL = $ConanRemoteUrl
+    }
+
+    if (-Not [string]::IsNullOrEmpty($ConanUsername)) {
+        $Env:CONAN_LOGIN_USERNAME = $ConanUsername
+    }
+
+    if (-Not [string]::IsNullOrEmpty($ConanPassword)) {
+        $Env:CONAN_PASSWORD = $ConanPassword
+    }
+
+    if (-Not (Test-Path Env:CONAN_REMOTE_NAME)) {
+        throw "CONAN_REMOTE_NAME environment variable must be set"
+    }
+
+    if (-Not (Test-Path Env:CONAN_REMOTE_URL)) {
+        throw "CONAN_REMOTE_URL environment variable must be set"
+    }
+
+    if (-Not (Test-Path Env:CONAN_LOGIN_USERNAME)) {
+        throw "CONAN_LOGIN_USERNAME environment variable must be set"
+    }
+    
+    if (-Not (Test-Path Env:CONAN_PASSWORD)) {
+        throw "CONAN_PASSWORD environment variable must be set"
+    }
+    
+    if ($MultiCache) {
+        $tarballs = Get-ChildItem . -Filter "*.tar.gz" -Recurse
+        $tarballs | ForEach-Object {
+            if (-Not (Test-Path $(Join-Path $_.Directory '.conan'))) {
+                tar -xf $_.FullName -C $_.Directory
+            }
+        }
+        
+        $ConanCaches = Get-ChildItem . -Filter ".conan" -Hidden -Recurse
+        
+        $ConanCaches | ForEach-Object {
+            $ConanUserHome = $_.Parent
+            $CacheName = $ConanUserHome.Name
+            Write-Host "Uploading $CacheName cache"
+            $Env:CONAN_USER_HOME="$ConanUserHome"
+            conan remote add $Env:CONAN_REMOTE_NAME $Env:CONAN_REMOTE_URL --force
+            conan user -p $Env:CONAN_PASSWORD -r $Env:CONAN_REMOTE_NAME $Env:CONAN_LOGIN_USERNAME
+            conan upload *@devolutions/stable --all --parallel -r $Env:CONAN_REMOTE_NAME -c
+        }
+    }
+}
+
+Invoke-TlkPublish @args
