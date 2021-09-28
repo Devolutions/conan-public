9c082c5ddf570d9f8b01267c90cb2cee90c92dd7
Marc-André Moreau <mamoreau@devolutions.net>
2021-09-28 14:56:26 -0400

    add conan user -p call

--- publish.ps1
+++ publish.ps1
@@ -61,6 +61,7 @@ function Invoke-TlkPublish {
         Write-Host "Uploading $CacheName cache"
         $Env:CONAN_USER_HOME="$ConanUserHome"
         conan remote add $Env:CONAN_REMOTE_NAME $Env:CONAN_REMOTE_URL --force
+        conan user -p
         conan upload *@devolutions/stable --all --parallel -r $Env:CONAN_REMOTE_NAME -c
 
         if ($LASTEXITCODE -ne 0) {
