3853fba35b9e13686fe138abcad7a49541924f0b
Richard Markiewicz <rmarkiewicz@devolutions.net>
2023-08-25 11:49:35 -0400

    conan-public: try to disable parallel uploads

--- publish.ps1
+++ publish.ps1
@@ -58,7 +58,7 @@ function Invoke-TlkPublish {
         $Env:CONAN_USER_HOME="$ConanUserHome"
         conan remote add $Env:CONAN_REMOTE_NAME $Env:CONAN_REMOTE_URL --force
         conan user $Env:CONAN_LOGIN_USERNAME -r $Env:CONAN_REMOTE_NAME -p
-        conan upload *@devolutions/stable --all --parallel -r $Env:CONAN_REMOTE_NAME -c
+        conan upload *@devolutions/stable --all -r $Env:CONAN_REMOTE_NAME -c
     }
 }
 
