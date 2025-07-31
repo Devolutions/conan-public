4cd2eeddca3849eb96035982fedf39fd4d964825
Marc-André Moreau <mamoreau@devolutions.net>
2025-07-31 15:26:28 -0400

    Fix cbake Git clone - use keyword arguments for Conan 2.x

--- recipes/cbake/conanfile.py
+++ recipes/cbake/conanfile.py
@@ -21,7 +21,7 @@ class CBake(ConanFile):
         folder = self.name
         self.output.info('Cloning repo: %s dest: %s branch: %s' % (self.url, folder, self.branch))
         git = Git(self, folder=folder)
-        git.clone(self.url, self.branch)
+        git.clone(url=self.url, branch=self.branch)
 
     def package(self):
         copy(self, '*', src=os.path.join(self.source_folder, 'cbake'), dst=self.package_folder, keep_path=True)
